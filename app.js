41166f8597b10dca87d0ecb2036869e768bc4cda
Kelly <babymis0628@gmail.com>
2022-02-23 22:15:34 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const app = express()
 
-const port = 3000
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/todo-list')
+const schema = mongoose.Schema
+const todoSchema = new schema ({
+  name: {
+    type: String,
+    required: true
+  }
+})
+module.exports = mongoose.model('Todo',todoSchema)
+
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 app.get('/',(req,res) => {
   res.send('success')
 })
 
-app.listen(port, () => {
-  console.log(`Express is running on http://localhost:${port}`)
+app.listen(3000, () => {
+  console.log(`Express is running on http://localhost:3000`)
 })
