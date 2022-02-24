d86921514432431b969a0a26f06ef2269b670302
Kelly <babymis0628@gmail.com>
2022-02-25 00:02:56 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.set('view engine', 'handlebars') // 啟用樣版引擎
 const Todo = require('./models/todo')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/todo-list')
+// body-parser
+const bodyParser = require('body-parser')
+
+
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -20,12 +24,24 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.use(bodyParser.urlencoded({extended: true})) // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 
 app.get('/',(req,res) => {
   Todo.find() // 從資料庫找資料：取出Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(todos => res.render('index',{todos})) // 將資料傳給index樣板
     .catch(error => console.error(error)) // 錯誤處理
+})
+
+app.get('/todos/new', (req,res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req,res) => {
+  const name = req.body.name // 從 req.body 拿出表單裡的name資料
+  return Todo.create({name}) // 存入資料庫
+          .then(() => res.redirect('/')) // 新增完成後導回首頁
+          .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
