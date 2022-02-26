0641971c573717d5286d89c869368db15c6b24b7
tech-kelly <babymis0628.mg10@nycu.edu.tw>
2022-02-26 15:18:40 +0800

    feat:add detail page

--- app.js
+++ app.js
@@ -51,11 +51,23 @@ app.get('/todos/:id', (req,res) => {
             .catch(error => console.error(error)) 
 })
 
-app.get('/todos/:id' , (req, res) => {
-  const id = req.params.id 
+app.get('/todos/:id/edit' ,(req,res) => {
+  const id = req.params.id
   return Todo.findById(id)
             .lean()
-            .then((todo))
+            .then((todo) => res.render('edit',{todo}))
+            .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit' , (req, res) => {
+  const id = req.params.id 
+  const name = req.body.name
+  return Todo.findById(id)
+            .then(todo => {
+              todo.name = name
+              return todo.save()
+            })
+            .then(() => res.redirect(`/todos/${id}`))
             .catch(error => console.error)
 })
 
