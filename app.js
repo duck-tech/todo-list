22e338a187995bc9752933c680680e2399dc0daf
tech-kelly <babymis0628.mg10@nycu.edu.tw>
2022-02-27 20:26:38 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.static('public'))
 app.get('/',(req,res) => {
   Todo.find() // 從資料庫找資料：取出Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({_id: 'asc'}) // 根據_id升冪排序
     .then(todos => res.render('index',{todos})) // 將資料傳給index樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
