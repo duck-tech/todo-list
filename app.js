6cf052eda28f93988521c9e122c2c6adea2bab6c
tech-kelly <babymis0628.mg10@nycu.edu.tw>
2022-03-04 08:31:31 +0800

    feat: add bcrypt.js

--- app.js
+++ app.js
@@ -11,8 +11,11 @@ const routes = require('./routes')
 require('./config/mongoose')
 
 // 使用者登入
+const flash = require('connect-flash')
 const session = require('express-session')
 const usePassport = require('./config/passport')
+
+
 
 // 如果在 Heroku 環境則使用 process.env.PORT
 const PORT = process.env.PORT || 3000
@@ -30,12 +33,15 @@ app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({extended: true})) // 用 app.use 指定每一筆請求都需要透過 body-parser 進行前置處理
 
 usePassport(app)
+app.use(flash())
 
 // 這個middleware會做用於所有路由
 app.use((req,res,next) => {
   //res.locals：所有樣板都可以使用的變數
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 app.use(routes)
--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,7 @@ module.exports = {
     if(req.isAuthenticated()){ // passport.js 提供的函示,會根據 request 的登入狀態回傳 true 或 false。
       return next()
     }
+    req.flash('warning_msg','請先登入才能使用！')
     res.redirect('/users/login')
   }
 }
